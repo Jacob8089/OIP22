7d59196574311040c8a59e6fe5eb8fa35f580f29
Jacob8089 <65939087+Jacob8089@users.noreply.github.com>
2022-12-05 14:58:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+
+    page.add(
+        ft.Text("Slider with value:"),
+        ft.Slider(value=1),
+        ft.Text("Slider with a custom range and label:"),
+        ft.Slider(min=0, max=255, divisions=255, label="{value}%",on_change=plus_click),
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ))
+    
+
+ft.app(target=main)
